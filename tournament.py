af9cf5ce7ccfb4bd5f1604367614ae74ffde8efb
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-02-11 00:41:42 +0100

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -12,18 +12,15 @@ from soccersimulator import SoccerTeam
 def get_team(nb_players ):
     team = SoccerTeam ( name = "equipe 3701195" )
     if nb_players == 1:
-        team.add(" Striker " , FonceurStrategy())
+        team.add(" Striker " , Gardien_v2())
     if nb_players == 2:
-        team.add(" Fonceur " , FonceurStrategy())
-        team.add(" Random " ,RandomStrategy())
-        return team
-
+        team.add(" Gardien " , Gardien_v2())
+        team.add(" Attaquant " ,Attaquant_v2())
+    return team
     
 if __name__ == "__main__":
-
-    from soccersimulator import Simulation , show_simu
-    # Check teams with 1 player and 2 players
-    team1 = get_team(1)
-    team2 = get_team(2)
-    # Create a match
-    simu = Simulation(team1 , team2 )
+	from soccersimulator import Simulation, show_simu
+	team1 = get_team(1)
+	team2 = get_team(2)
+	simu = Simulation(team1,team2)
+	show_simu(simu)
