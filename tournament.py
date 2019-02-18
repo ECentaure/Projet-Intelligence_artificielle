c260fd7040cf3dada33baee2233986e7cf07f7a1
ECentaure <46897816+ECentaure@users.noreply.github.com>
2019-02-18 12:16:13 +0100

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -9,14 +9,7 @@ from sanstitre0 import *
 
 from soccersimulator import SoccerTeam
 
-def get_team(nb_players ):
-    team = SoccerTeam ( name = "equipe 3701195" )
-    if nb_players == 1:
-        team.add(" Striker " , Attaquant_v2())
-    if nb_players == 2:
-        team.add(" Gardien " , Gardien_v2())
-        team.add(" Attaquant " ,Attaquant_v2())
-    return team
+
     
 if __name__ == "__main__":
 	from soccersimulator import Simulation, show_simu
