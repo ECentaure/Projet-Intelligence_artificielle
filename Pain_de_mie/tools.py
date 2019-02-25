de27afa67812139b42ca456c929ab4b1177477b5
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-02-25 19:48:04 +0100

    Add files via upload

--- Pain_de_mie/tools.py
+++ Pain_de_mie/tools.py
@@ -39,6 +39,146 @@ class SuperState ( object ):
     @property
     def goal(self):
         return Vector2D((self.id_team - 1)*settings.GAME_WIDTH , settings.GAME_HEIGHT / 2)
+    
+    
+    
+    
+    def id_team_adv(self):
+        if(self.id_team ==1):
+            return 2
+        return 1
+    
+    
+    def distance_j_b(self):
+        return self.player.distance(self.ball)
+     
+        
+    def tirer_ou_pas(self):
+        return self.distance_j_b() < PLAYER_RADIUS + BALL_RADIUS
+    
+    def aller_courrir(self,p):
+        """if (self.posi_ball().x==settings.GAME_WIDTH/2) and (self.posi_ball().y==settings.GAME_HEIGHT/2):
+            return SoccerAction(Vector2D(),Vector2D())
+        else:"""
+        return SoccerAction(acceleration = p-self.player,shoot = Vector2D())
+    
+    def aller_marcher(self,p):                                                    
+        v1=p-self.player
+        v1.norm=0.05
+        return SoccerAction(acceleration = v1,shoot =Vector2D())
+    
+    def aller_courrir_marcher(self,p):
+        if (self.distance_j_vect(p)<3):    
+            return self.aller_marcher(p)
+        return self.aller_courrir(p)
+        
+    def ball_34(self):
+        if self.id_team_adv() == 2:
+            if (self.ball.x>(3*settings.GAME_WIDTH)/4):
+                return 0                                                           #Attaque
+            else:
+                return 1                                                           #Defense
+        if (self.id_team_adv() == 1):
+            if (self.ball.x<(settings.GAME_WIDTH)/4):
+                return 0                                                           #Attaque
+            else:
+                return 1  
+        
+    def mini_shoot(self,p):
+        return SoccerAction(Vector2D(),(p-self.player)*0.03)
+      
+    def ally_vitesse(self):
+        if (self.id_team==1):
+            if (self.id_player==0):
+                return self.state.player_state(1,1).vitesse
+            if (self.id_player==1):
+                return self.state.player_state(1,0).vitesse
+        if (self.id_team ==2):
+            if (self.id_player==0):
+                return self.state.player_state(2,1).vitesse
+            if (self.id_player ==1):
+                return self.state.player_state(2,0).vitesse
+            
+        
+            
+    
+    def shoot_but(self,p):
+        if (self.ball_34()==0):
+            return SoccerAction(Vector2D(),(p-self.player)*0.1)
+        else:
+            return self.mini_shoot(p) 
+    
+        
+    def decoupage_terrain(self):
+        j = self.player
+        X_case = math.floor(((j.x)/(150))*4)
+        Y_case = math.floor(((j.y)/(90))*3)
+        return (X_case,Y_case)
+    
+    def ally_position(self):
+        if (self.id_team==1):
+            if (self.id_player==0):
+                return self.state.player_state(1,1).position
+            if (self.id_player==1):
+                return self.state.player_state(1,0).position
+        if (self.id_team==2):
+            if (self.id_player==0):
+                return self.state.player_state(2,1).position
+            if (self.id_player==1):
+                return self.state.player_state(2,0).position
+    
+    
+    def shoot(self,p):
+        return SoccerAction(Vector2D(),(p-self.player))
+    
+    def je_suis_dans_mon_camp(self):
+        if (self.id_team==1) and (self.player.x<=settings.GAME_WIDTH/2):
+            return True
+        else:
+            return False
+        if (self.id_team==2) and (self.player.x>=settings.GAME_WIDTH/2):
+            return True
+        else:
+            return False 
+    
+    def aller_gardien(self,p):
+        if (self.id_team==1):
+            if (self.ball.x+self.state.ball.vitesse.x>=(settings.GAME_WIDTH)/3) :
+                v3=self.ball-self.goalAdv
+                v3.norm=8.5
+                v2= Vector2D(0,settings.GAME_HEIGHT/2)
+                return self.aller_courrir_marcher(v2+v3)       
+            else:
+                return self.aller_courrir_marcher(p) 
+        if (self.id_team==2):
+            if(self.ball.x+self.state.ball.vitesse.x<=(2*settings.GAME_WIDTH)/3):
+                v3=self.ball-self.goalAdv
+                v3.norm=8.5
+                v2= Vector2D(settings.GAME_WIDTH,settings.GAME_HEIGHT/2)
+                return self.aller_courrir_marcher(v2+v3) 
+            else:  
+                return self.aller_courrir_marcher(p)
+            
+            
+    def aller_vers(self, vecteur):
+        """ renvoie le vecteur déplacement entre le joueur et le point x, y présent dans le champ vecteur"""
+        return vecteur-self.player
+    
+    def distance_j_vect(self,vecteur):
+        return vecteur.distance(self.player)
+        
+    def Immobile(self):
+        return SoccerAction(Vector2D(0,0), Vector2D(0,0))  
+    
+    def distance_joueur_but(self):
+        if (self.id_team == 1):
+            but = Vector2D( 0, GAME_HEIGHT/2 )
+        else:
+            but = Vector2D( GAME_WIDTH, GAME_HEIGHT/2)
+        
+        return self.player.distance(but)
+    
+   
 
 def liste_joueur(state, id_team): #donne une liste de state de tout les joueurs
     return [(it , ip) for (it, ip) in state.players if it == id_team]
@@ -82,11 +222,6 @@ def distanceBut(state, id_team, id_player):
     return dist    
 
 
-def id_team_adv(self):
-    if(self.id_team ==1):
-        return 2
-    return 1
-
 def deplaceVers(A, id_joueur):
     player = state.player_state(id_team, id_player).position
     return SoccerAction(acceleration = A - player)
@@ -106,136 +241,6 @@ def gogetter(state):
         return SoccerAction (acceleration = state.ball - state.player )        
 
     
-def aller_vers(self, vecteur):
-    """ renvoie le vecteur déplacement entre le joueur et le point x, y présent dans le champ vecteur"""
-    return vecteur-self.player
-
-def distance_j_vect(self,vecteur):
-    return vecteur.distance(self.player)
-    
-def Immobile(self):
-    return SoccerAction(Vector2D(0,0), Vector2D(0,0))  
-
-def distance_joueur_but(self):
-    if (self.id_team == 1):
-        but = Vector2D( 0, GAME_HEIGHT/2 )
-    else:
-        but = Vector2D( GAME_WIDTH, GAME_HEIGHT/2)
-    
-    return self.player.distance(but)
-
-
-def distance_j_b(self):
-    return self.player.distance(self.ball)
- 
-    
-def tirer_ou_pas(self):
-    return self.distance_j_b() < PLAYER_RADIUS + BALL_RADIUS
-
-def aller_courrir(self,p):
-    """if (self.posi_ball().x==settings.GAME_WIDTH/2) and (self.posi_ball().y==settings.GAME_HEIGHT/2):
-        return SoccerAction(Vector2D(),Vector2D())
-    else:"""
-    return SoccerAction(acceleration = p-self.player,shoot = Vector2D())
-
-def aller_marcher(self,p):                                                    
-    v1=p-self.player
-    v1.norm=0.05
-    return SoccerAction(acceleration = v1,shoot =Vector2D())
-
-def aller_courrir_marcher(self,p):
-    if (self.distance_j_vect(p)<3):    
-        return self.aller_marcher(p)
-    return self.aller_courrir(p)
-    
-def ball_34(self):
-    if self.id_team_adv() == 2:
-        if (self.ball.x>(3*settings.GAME_WIDTH)/4):
-            return 0                                                           #Attaque
-        else:
-            return 1                                                           #Defense
-    if (self.id_team_adv() == 1):
-        if (self.ball.x<(settings.GAME_WIDTH)/4):
-            return 0                                                           #Attaque
-        else:
-            return 1  
-    
-def mini_shoot(self,p):
-    return SoccerAction(Vector2D(),(p-self.player)*0.03)
-  
-def ally_vitesse(self):
-    if (self.id_team==1):
-        if (self.id_player==0):
-            return self.state.player_state(1,1).vitesse
-        if (self.id_player==1):
-            return self.state.player_state(1,0).vitesse
-    if (self.id_team ==2):
-        if (self.id_player==0):
-            return self.state.player_state(2,1).vitesse
-        if (self.id_player ==1):
-            return self.state.player_state(2,0).vitesse
-        
-    
-        
-
-def shoot_but(self,p):
-    if (self.ball_34()==0):
-        return SoccerAction(Vector2D(),(p-self.player)*0.1)
-    else:
-        return self.mini_shoot(p) 
-
-    
-def decoupage_terrain(self):
-    j = self.player
-    X_case = math.floor(((j.x)/(150))*4)
-    Y_case = math.floor(((j.y)/(90))*3)
-    return (X_case,Y_case)
-
-def ally_position(self):
-    if (self.id_team==1):
-        if (self.id_player==0):
-            return self.state.player_state(1,1).position
-        if (self.id_player==1):
-            return self.state.player_state(1,0).position
-    if (self.id_team==2):
-        if (self.id_player==0):
-            return self.state.player_state(2,1).position
-        if (self.id_player==1):
-            return self.state.player_state(2,0).position
-
-
-def shoot(self,p):
-    return SoccerAction(Vector2D(),p-self.player)
-
-def je_suis_dans_mon_camp(self):
-    if (self.id_team==1) and (self.player.x<=settings.GAME_WIDTH/2):
-        return True
-    else:
-        return False
-    if (self.id_team==2) and (self.player.x>=settings.GAME_WIDTH/2):
-        return True
-    else:
-        return False 
-
-def aller_gardien(self,p):
-    if (self.id_team==1):
-        if (self.ball.x+self.state.ball.vitesse.x>=(settings.GAME_WIDTH)/3) :
-            v3=self.ball-self.goalAdv
-            v3.norm=8.5
-            v2= Vector2D(0,settings.GAME_HEIGHT/2)
-            return self.aller_courrir_marcher(v2+v3)       
-        else:
-            return self.aller_courrir_marcher(p) 
-    if (self.id_team==2):
-        if(self.ball.x+self.state.ball.vitesse.x<=(2*settings.GAME_WIDTH)/3):
-            v3=self.ball-self.goalAdv
-            v3.norm=8.5
-            v2= Vector2D(settings.GAME_WIDTH,settings.GAME_HEIGHT/2)
-            return self.aller_courrir_marcher(v2+v3) 
-        else:  
-            return self.aller_courrir_marcher(p)
    
-
-
-
+    
 
