1be56a3f76b9958f3841b9f8ec8221fd918cce86
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-03-18 00:25:52 +0100

    Update __init__.py

--- Pain_de_mie/__init__.py
+++ Pain_de_mie/__init__.py
@@ -4,8 +4,13 @@ from .Strategies import *
 def get_team(nb_players ):
     team = SoccerTeam ( name = "equipe 3701195" )
     if nb_players == 1:
-        team.add(" Striker " , Attaquant_v2())
+        team.add(" Striker " , Strat_switch())
     if nb_players == 2:
         team.add(" Gardien " , Gardien_v2())
         team.add(" Attaquant " ,Attaquant_v2())
+    if nb_players == 4:
+        team.add(" Gardien " , Gardien_v2())
+        team.add(" Attaquant " ,Attaquant_v2())
+        team.add(" Striker " , Strat_switch())
+        teeam.add("Go",FonceurStrategy())
     return team
