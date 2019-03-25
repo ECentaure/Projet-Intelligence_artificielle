590ad8a4e7a2bd519c4063c37704d68be9979456
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-03-25 01:04:04 +0100

    Update tournament.py

--- tournament.py
+++ tournament.py
@@ -13,7 +13,7 @@ from soccersimulator import SoccerTeam
     
 if __name__ == "__main__":
 	from soccersimulator import Simulation, show_simu
-	team1 = get_team(1)
-	team2 = get_team(2)
+	team1 = get_team(4)
+	team2 = get_team(4)
 	simu = Simulation(team1,team2)
 	show_simu(simu)
