5f033a2b41ba8eebbb5997c08c3b7bc5c9c0df0f
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-05-09 12:53:03 +0200

    Add files via upload

--- sanstitre0.py
+++ sanstitre0.py
@@ -47,7 +47,7 @@ class FonceurStrategy(Strategy):
         Strategy.__init__(self, "Fonceur")
 
     def compute_strategy(self, state, id_team, id_player):
-        s = SuperState(state, id_team, id_player)
+        s = tools.SuperState(state, id_team, id_player)
         dist = distance(state, id_team, id_player, s.ball)
             
         if( dist < settings.PLAYER_RADIUS + settings.BALL_RADIUS): 
@@ -61,7 +61,7 @@ class GardienStrategy(Strategy):
 
     def compute_strategy(self, state, id_team, id_player):
         
-        s = SuperState(state, id_team, id_player)
+        s = tools.SuperState(state, id_team, id_player)
         dist = distance(state, id_team, id_player,s.ball)
         
         if(dist < 10):
@@ -84,6 +84,14 @@ class Defenseur(Strategy):
         mystate = tools.SuperState(state,idteam,idplayer)
         myaction= tools.Action(mystate)
         return myaction.defense()
+    
+class Milieu(Strategy):
+    def __init__(self,name="defense"):
+        Strategy.__init__(self,name)
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        return myaction.action_milieu()
 
 class Defenseur2(Strategy):
     def __init__(self,name="defense"):
@@ -101,34 +109,7 @@ class Attaquant4(Strategy):
         myaction= tools.Action(mystate)
         return myaction.action_attaquant4()
    
-class Gardien(Strategy):
-    """_/!\_ adapter pour du deux contre deux"""
-    def __init__(self):
-        Strategy.__init__(self, "Gardien")
 
-    def compute_strategy(self, state, id_team, id_player):
-        # id_team is 1 or 2
-        # id_player starts at 0
-        position_ball = state.ball.position
-        position_joueur = state.player_state(id_team,id_player).position
-        but1 = Vector2D(3, GAME_HEIGHT/2)
-        but2 = Vector2D(GAME_WIDTH-3, GAME_HEIGHT/2)
-        if (id_team == 2):
-            new_position_joueur = but2 - position_joueur
-        else:
-            new_position_joueur = but1 - position_joueur
-        w = position_ball - new_position_joueur
-        if( id_team == 2 and w.norm < 5 or (state.ball.position+5*state.ball.vitesse).x>110):
-            tir =(but1-position_ball)
-            tir.angle -= 0.7853
-            return SoccerAction(acceleration = (position_ball-position_joueur)*maxPlayerAcceleration,shoot=tir*3)
-      
-        if( id_team == 1 and w.norm < 5 or state.ball.position.x<40):
-            tir=(but2-position_ball)
-            tir.angle -= 0.7853
-            return SoccerAction(acceleration = (position_ball-position_joueur)*maxPlayerAcceleration,shoot=tir*3)
-        else:
-            return SoccerAction(acceleration = new_position_joueur)
         
 class Attaquant_v2(Strategy):
     def __init__(self):
@@ -141,25 +122,15 @@ class Attaquant_v2(Strategy):
         if (fct.tirer_ou_pas()):
             return fct.aller_courrir_marcher(ball + 5*state.ball.vitesse) + fct.shoot_but(but)
         else:
-            return fct.aller_courrir_marcher(ball)   
- 
-class Gardien_v2(Strategy):
-    def __init__(self):
-        Strategy.__init__(self,"Attaquantv_2")
+            return fct.aller_courrir_marcher(ball)  
         
-    def compute_strategy(self,state,id_team,id_player):
-        fct = SuperState(state,id_team,id_player)
-        ball = state.ball.position 
-        if (fct.tirer_ou_pas()):
-            return fct.aller_gardien(ball + 5*state.ball.vitesse)+fct.shoot(fct.ally_position()+fct.ally_vitesse())    
-        else:
-            if fct.je_suis_dans_mon_camp():
-                if (ball.distance(fct.player)<10):
-                    return fct.aller_courrir_marcher(ball)
-                else:
-                    return fct.aller_gardien(ball + 5*state.ball.vitesse)   
-            else:
-                return fct.aller_gardien(ball + 5*state.ball.vitesse)+fct.shoot(fct.ally_position()+fct.ally_vitesse())   
+class Campeur(Strategy):
+    def __init__(self,name="campeur"):
+        Strategy.__init__(self,name)
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        return myaction.action_campeur()
 
 
 class Strat_switch(Strategy):
@@ -193,8 +164,7 @@ class Strat_switch(Strategy):
        
         s = SuperState(state, id_team, id_player)
      
-        
-        if(joueur_proche_objet(state,id_team,id_player,state.ball.position))>30 and joueur_proche_objet(state,id_team_adv(id_team),id_player,s.goal)<10 and joueur_proche_objet(state,id_team,id_player,s.goal)<20:
+        if(s.ball_camp()):
            return self.compute_strategy_def( state, id_team, id_player)
         else:
            return self.compute_strategy_attack( state, id_team, id_player)
@@ -204,12 +174,15 @@ team1 = SoccerTeam(name="Team 1")
 team2 = SoccerTeam(name="Team 2")
 
 # Add players
-team2.add("kiwi",Strat_switch()) 
+team2.add("kiwi",Defenseur2()) 
+team2.add("kiwi2",Defenseur2()) 
 #team2.add("paille", Defenseur()) 
+team2.add("Fonceur", FonceurStrategy())  
 team2.add("kiwi",GardienStrategy()) 
 team1.add("Defenseur", Defenseur())   
 team1.add("Attaquant", Attaquant4()) 
-team1.add("Fonceur", FonceurStrategy())  
+team1.add("Campeur", Campeur()) 
+team1.add("Defenseur2", Defenseur2())   
 #team1.add("strat",Strat_switch()) 
  
 
