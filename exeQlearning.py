be4f9628fe4109d853256e55b1ff11bf11418bc3
AssetouN <46898878+AssetouN@users.noreply.github.com>
2019-05-09 12:53:38 +0200

    Add files via upload

--- exeQlearning.py
+++ exeQlearning.py
@@ -5,23 +5,85 @@ Created on Mon Mar 25 16:16:19 2019
 
 @author: emeline
 """
+import random
+import pickle as pkl
+from Qlearning import *
+from QStrategy import *
+from Pain_de_mie import *
+from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam, Simulation, show_simu,settings
 
-from socceria import QLearning , RandomPos , QStrategy
+
+class FrappeStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Frappe")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        if mystate.peut_frapper():
+            return myaction.shoot_but()
+        else :
+            return myaction.aller_vers_balle()
+        
+        
+
+class DribbleStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Dribble")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        if (mystate.peut_frapper()):
+            return myaction.pousse_ball()
+        else :    
+            return myaction.aller_vers_balle()
+            
+
+class PasseStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Passe")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        if (mystate.peut_frapper()):
+            return myaction.passe_2v2()
+        else :    
+            return myaction.aller_vers_balle()
+
+class Replacement_attaquant_Strategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Replacement offensif")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        return myaction.replacement_attaquant4()
+
+class Replacement_defense_Strategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Replacement defensif")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = tools.SuperState(state,idteam,idplayer)
+        myaction= tools.Action(mystate)
+        return myaction.replacement_defense2()
+
+
+
+
 # Strategy
-QTestStrategy = QStrategy ()
-QTestStrategy . add ( " right " , SimpleStrategy ( shoot_right , "" ))
-QTestStrategy . add ( " left " , SimpleStrategy ( shoot_left , "" ))
-QTestStrategy . add ( " up " , SimpleStrategy ( shoot_up , "" ))
-QTestStrategy . add ( " down " , SimpleStrategy ( shoot_down , "" ))
+QTestStrategy = QStrategy()
+QTestStrategy.add ( " Frappe " , FrappeStrategy())
+QTestStrategy.add ( " Dribble " , DribbleStrategy())
+QTestStrategy.add ( " Replacement_Attaque " , Replacement_attaquant_Strategy())
 # Learning
-expe = QLearning ( strategy = QTestStrategy , monte_carlo = False )
-expe . start ( fps =1500)
-with open ( " qstrategy . pkl " , "wb ") as fo :
-    QTestStrategy . qtable = expe . qtable
-    pkl . dump ( QTestStrategy , fo )
-# Test
-with open ( " qstrategy . pkl .351 " , " rb ") as fi :
-    QStrategy = pkl . load ( fi )
-    # Simulate and display the match
-    simu = RandomPos ( QStrategy )
-    simu . start ()
+expe = QLearning(strategy = QTestStrategy)
+expe.start()
+with open ('qstrategy.txt','wb') as fo :
+    QTestStrategy.qtable = expe.qtable
+    pkl.dump(QTestStrategy , fo )
+    print(fo)
+    
+# Tes
+with open ( 'qstrategy.pkl','rb') as fi :
+    QStrategy = pkl.load(fi)
+    print(QStrategy)
+    #simu = random.choice(QStrategy.strategy())
+    #simu.start()
